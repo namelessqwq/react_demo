40ccb0a9012b12f3c7fdca17566c657898fce8be
namelessqwq <2540636681@qq.com>
2021-01-25 18:38:07 +0800

    first commit

---
+++ src/ComponentList.js
@@ -0,0 +1,14 @@
+const { Component } = require("react");
+
+const list = { list }
+
+
+class ComponentList extends Component {
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default ComponentList
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ComponentApp from './ComponentApp.js'
+// import reportWebVitals from './reportWebVitals';
+
+
+
+
+ReactDOM.render(
+  <ComponentApp />,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
